8f2a2b99c3aa0d2350340e6b0303f0e27e716f41
Kỳ Đinh <dinhky2468@gmail.com>
2023-03-06 11:39:12 +0700

    build routes

--- configs/routes.php
+++ configs/routes.php
@@ -1,3 +1,7 @@
 <?php
 
 $routes['default_controller'] = 'home';
+
+$routes['danh-sach-san-pham'] = 'product/listProduct';
+$routes['danh-sach-san-pham/(.+)'] = 'error';
+$routes['san-pham/(.+)'] = 'product/detail/$1';
